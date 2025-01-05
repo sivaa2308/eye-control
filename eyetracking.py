b45ac7a0268c336018d663c2060f2fa690ccb810
reddy23233 <165468260+reddy23233@users.noreply.github.com>
2025-01-06 00:19:13 +0530

    eye direction detection
    make changes to detect the gaze detection. One step closer to the result . 6/1/2025

--- eyetracking.py
+++ eyetracking.py
@@ -5,6 +5,8 @@ from math import hypot
 
 font = cv2.FONT_HERSHEY_COMPLEX_SMALL
 
+data = []
+
 def midpoint(p1, p2):
     return int((p1.x + p2.x) / 2), int((p1.y + p2.y) / 2)
 
@@ -24,6 +26,46 @@ def blinking_ratio(eye_points, facial_landmarks):
     
     return hori_length / ver_length
 
+def get_gaze_ratio(eye_points , facial_landmarks):
+
+    left_eye_region = np.array([(facial_landmarks.part(eye_points[0]).x , facial_landmarks.part(eye_points[0]).y),
+                                (facial_landmarks.part(eye_points[1]).x , facial_landmarks.part(eye_points[1]).y),
+                                (facial_landmarks.part(eye_points[2]).x , facial_landmarks.part(eye_points[2]).y),
+                                (facial_landmarks.part(eye_points[3]).x , facial_landmarks.part(eye_points[3]).y),
+                                (facial_landmarks.part(eye_points[4]).x , facial_landmarks.part(eye_points[4]).y),
+                                (facial_landmarks.part(eye_points[5]).x , facial_landmarks.part(eye_points[5]).y)] , np.int32)
+
+    height, width, _ = frame.shape
+    mask = np.zeros((height, width), np.uint8)
+    cv2.polylines(mask, [left_eye_region], True, 255, 2)
+    cv2.fillPoly(mask, [left_eye_region], 255)
+    eye = cv2.bitwise_and(gray, gray, mask=mask)
+
+    min_x = np.min(left_eye_region[:,0])
+    max_x = np.max(left_eye_region[:,0])
+    min_y = np.min(left_eye_region[:,1])
+    max_y = np.max(left_eye_region[:,1])
+
+    gray_eye = eye[min_y: max_y, min_x: max_x]
+    _, threshold_eye = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY)
+
+    height , width = threshold_eye.shape
+    left_side_threshold = threshold_eye[0:height , 0:int(width/2)]
+    left_side_white = cv2.countNonZero(left_side_threshold)
+
+    right_side_threshold = threshold_eye[0 : height , int(width/2):width]
+    right_side_white = cv2.countNonZero(right_side_threshold)
+
+
+    if left_side_white==0:
+        gaze_ratio=1
+    elif right_side_white==0:
+        gaze_ratio=5
+    else:
+        gaze_ratio = left_side_white / right_side_white
+
+    return gaze_ratio
+
 def draw_text(frame, text, position, color=(255, 0, 0)):
     cv2.putText(frame, text, position, font, 1, color, 2)
 
@@ -35,6 +77,7 @@ cap = cv2.VideoCapture(0)
 BLINK_THRESHOLD = 5.7  # Adjust based on your use case
 while True:
     _, frame = cap.read()
+    new_frame= np.zeros((500,500,3) , np.uint8)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     faces = detector(gray)
     for face in faces:
@@ -47,19 +90,46 @@ while True:
         # Average blinking ratio
         blink_average = (left_eye_ratio + right_eye_ratio) / 2
 
-        if left_eye_ratio > BLINK_THRESHOLD and right_eye_ratio <= BLINK_THRESHOLD:
-            draw_text(frame, "Right Eye Close", (50, 150))
-        elif right_eye_ratio > BLINK_THRESHOLD and left_eye_ratio <= BLINK_THRESHOLD:
-            draw_text(frame, "Left Eye Close", (50, 110))
-        elif blink_average > BLINK_THRESHOLD:
+        if blink_average > BLINK_THRESHOLD:
             draw_text(frame, "Both Eyes Closed", (50, 50))
+            
+        #elif right_eye_ratio > BLINK_THRESHOLD and left_eye_ratio <= BLINK_THRESHOLD:
+         #   draw_text(frame, "Left Eye Close", (50, 110))
+        #elif left_eye_ratio > BLINK_THRESHOLD and right_eye_ratio <= BLINK_THRESHOLD:
+         #   draw_text(frame, "Right Eye Close", (50, 150))
         else:
             draw_text(frame, "Eyes Open", (50, 200), color=(0, 255, 0))
 
+        #gaze detection 
+
+        # Gaze detection
+        gaze_ratio_left_eye = get_gaze_ratio([36, 37, 38, 39, 40, 41], landmarks)
+        gaze_ratio_right_eye = get_gaze_ratio([42, 43, 44, 45, 46, 47], landmarks)
+        gaze_ratio = (gaze_ratio_right_eye + gaze_ratio_left_eye) / 2
+
+        #cv2.putText(frame , str(gaze_ratio) , (50, 50) , font , 2 , (255 , 0 , 0) , 2)
+
+        
+
+        if gaze_ratio <= 1:
+            cv2.putText(frame, "RIGHT", (50, 100), font, 2, (0, 0, 255), 3)
+            new_frame[:] = (0, 0, 255)
+           
+        elif 1 < gaze_ratio < 1.8:
+            cv2.putText(frame, "CENTER", (50, 100), font, 2, (0, 0, 255), 3)
+           
+        else:
+            new_frame[:] = (255, 0, 0)
+            cv2.putText(frame, "LEFT", (50, 100), font, 2, (0, 0, 255), 3)
+         
+        
+        #cv2.polylines(frame , [left_eye_region] , True , (255,0,0) , 2)
+
     cv2.imshow("Frame", frame)
+    cv2.imshow("new frame", new_frame)
     key = cv2.waitKey(1)
     if key == 27:  # ESC key
         break
-
+print(data)
 cap.release()
 cv2.destroyAllWindows()
